33034a2c49abb6937afd002f548b9021eb7ec019
Gokul0201 <kgokula314@gmail.com>
2022-11-12 11:09:27 +0530

    commit

--- dbConfig.js
+++ dbConfig.js
@@ -2,5 +2,5 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
 require("dotenv").config();
 const dburl = process.env.MONGO_URI;
-
-module.exports={mongodb,MongoClient,dburl}
+const dbname=process.env.MONGO_NAME;
+module.exports={mongodb,MongoClient,dburl,dbname}
--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,51 @@ var express = require('express');
 const { default: mongoose } = require('mongoose');
 var router = express.Router();
 const {Movie,Ticket} = require('../library/auth')
-
+const {mongodb,MongoClient,dburl,dbname}= require('../dbConfig')
 router.get('/',async(req,res)=>{
   res.send("Server Running Sucessfully")
 })
 
 /* GET home page. */
-router.post('/postmovie',async(req,res)=>{
-  let data = await Movie.create(req.body);
-  if(data){
-    rs.status(201).json({
-      message:'Movie Created Successfully',
-      data
+// router.post('/postmovie',async(req,res)=>{
+//   let data = await Movie.insertone(req.body);
+//   if(data){
+//     res.status(201).json({
+//       message:'Movie Created Successfully',
+//       data
       
 
-  })
+//   })
+//   }
+// })
+
+const client = new MongoClient(dburl)
+router.post('/postmovie',async(req, res, next) => {
+  await client.connect();
+  try {
+  
+    const db = await client.db(dbname);
+    let users = await db.collection('movies').insertOne(req.body)
+    
+        res.send({
+          statusCode: 200,
+          message:"movie added successfully",
+          users
+        })
+   
+  } catch (error) {
+    console.log(error)
+    res.send({ 
+      statusCode:500,
+      message:"Internal Server Error",
+      error
+    })
   }
-})
+  finally{
+    client.close()
+  }
+});
+
 
 router.post('/postticket',async(req,res)=>{
   let data = await Ticket.create(req.body);
